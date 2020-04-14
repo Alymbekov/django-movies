ddff93fd5f3c6458b80e403a74dc6e9b04b07dd4
Alymbekov <maxim.makarov.1997@mail.ru>
2020-04-14 13:38:19 +0600

    added details view and also templates for this

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.MoviesView.as_view())
+    path("", views.MoviesView.as_view()),
+    path("movies/<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.base import View
 
 
-from .models import Movie
+from .models import Movie, Genre
 
 
 class MoviesView(View):
@@ -10,3 +10,11 @@ class MoviesView(View):
     def get(self, request):
         movies = Movie.objects.all()
         return render(request, "movies/movies.html", {"movie_list": movies})
+
+
+class MovieDetailView(View):
+    """ Полное описание фильма"""
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
+        genres = Genre.objects.all()
+        return render(request, "movies/movie_detail.html", {"movie": movie, "genres": genres})
