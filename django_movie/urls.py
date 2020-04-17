a8e9baa6096a940b57a7511680851b2367f44040
Alymbekov <maxim.makarov.1997@mail.ru>
2020-04-17 13:49:18 +0600

    added ckeditor to movie form and add youtube plugin to add trailer with  panel

--- django_movie/urls.py
+++ django_movie/urls.py
@@ -19,8 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("movies.urls")),
+    path('', include('movies.urls')),
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 if settings.DEBUG:
