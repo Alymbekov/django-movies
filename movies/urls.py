dafd1e257a0036c36c715a5e147a76e7a7d41e6e
Alymbekov <maxim.makarov.1997@mail.ru>
2020-04-19 15:25:52 +0600

    add ajax filters movies

--- movies/urls.py
+++ movies/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("", views.MoviesView.as_view()),
     path("movies/<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
     path("movies/review/<int:pk>/", views.AddReview.as_view(), name="add_review"),
+    path("filter/", views.FilterMoviesView.as_view(), name="filter"),
+    path("json-filter/", views.JsonFolterMoviesView.as_view(), name="json_filter"),
     path("actor/<str:slug>/", views.ActorView.as_view(), name="actor_detail"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.db.models import Q
+from django.http import JsonResponse
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from .forms import ReviewForm
 
-from .models import Movie, Genre, Actor
+from .models import Movie, Genre, Actor, Genre
 
 
-class MoviesView(ListView):
+class GenreYearMixin():
+    """Жанры и года выхода фильмов"""
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MoviesView(GenreYearMixin, ListView):
     """Список фильмов"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYearMixin, DetailView):
     """ Полное описание фильма"""
     model = Movie
     slug_field = "url"
@@ -38,8 +49,34 @@ class AddReview(View):
         return redirect(reverse_lazy("movie_detail", kwargs={"slug": movie.url}))
 
 
-class ActorView(DetailView):
+class ActorView(GenreYearMixin, DetailView):
     """Вывод инфы о актере"""
     model = Actor
     template_name = "movies/actor.html"
     slug_field = "name"
+
+
+class FilterMoviesView(GenreYearMixin, ListView):
+    """Фильтр фильмов"""
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |
+            Q(genres__in=self.request.GET.getlist("genre")) 
+
+        )
+        return queryset
+
+class JsonFolterMoviesView(ListView):
+    """Фильтр фильмов в json"""
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |
+            Q(genres__in=self.request.GET.getlist("genre")) 
+        ).distinct().values("title", "tagline", "url", "poster")
+        return queryset
+    
+    def get(self, request, *args, **kwargs):
+        queryset = list(self.get_queryset())
+        return JsonResponse({"movies": queryset}, safe=False)
+     
+    
